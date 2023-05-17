676bb9e0d4da84bc4f3766ab3f75ab59e17ade16
marcegdv <mgdv79@gmail.com>
2023-05-16 22:49:37 -0300

    glassy menu

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+    return (
+        <div className='container'>
+            <MenuOptions />
+            <MenuOptions />
+            <MenuOptions />
+            <MenuOptions />
+            <MenuOptions />
+            <MenuOptions />
+        </div>
+    );
+
+}
+
+export default App;
+
+
+
+function MenuOptions({ optionList }) {
+
+    const options = optionList || [
+        'New',
+        'Open...',
+        '-',
+        'Save',
+        'Save as...',
+        '-',
+        'Close',
+    ];
+
+    return (
+        <div className='menu-container'>
+            <div className='menu-options-contianer'>
+                {options.map(option =>
+                    <MenuOption text={option} key={option} onClick={() => alert(`Clicked ${option}!`)} />
+                )}
+            </div>
+        </div>
+    );
+
+};
+
+function MenuOption({ text, onClick }) {
+
+    const handleOnClick = () => {
+        if (onClick) onClick();
+    };
+
+    return (
+        <>
+            {text === '-'
+                ? <div className='option-separator'></div>
+                : <div className='option-container' onClick={handleOnClick}>
+                    <span className='option-text'>{text}</span>
+                </div>
+            }
+        </>
+    );
+
+};
